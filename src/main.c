b94b3608ba1c3919c585f13510e095aab5dafa37
Mike Burns <mike@mike-burns.com>
2014-07-29 18:48:29 +0200

    Resize the initial window
    This introduces the `DEFAULT_HEIGHT` and `DEFAULT_WIDTH` constants.
    
    Also fix the formatting. This project follows OpenBSD's KNF, as best it
    can.
    
    Also extract an `INTERFACE_PATH` constant.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,9 @@
 #include <gtk/gtk.h>
 
+#define INTERFACE_PATH "/home/mike/argonaut/argonaut.ui"
+#define DEFAULT_HEIGHT 350
+#define DEFAULT_WIDTH 700
+
 int
 main(int argc, char *argv[]) {
 	GtkBuilder *builder;
@@ -7,11 +11,12 @@ main(int argc, char *argv[]) {
 
 	gtk_init(&argc, &argv);
 
-        builder = gtk_builder_new_from_file("/home/mike/argonaut-py/argonaut.ui");
-        window = GTK_WIDGET(gtk_builder_get_object(builder, "directory-window"));
+	builder = gtk_builder_new_from_file(INTERFACE_PATH);
+	window = GTK_WIDGET(gtk_builder_get_object(builder, "directory-window"));
 	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+	gtk_window_set_default_size(GTK_WINDOW(window), DEFAULT_WIDTH, DEFAULT_HEIGHT);
 
-        gtk_widget_show(window);
+	gtk_widget_show(window);
 
 	gtk_main ();
 
