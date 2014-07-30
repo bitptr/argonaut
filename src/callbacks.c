af40d059eec621462386945a2777647600b515c2
Mike Burns <mike@mike-burns.com>
2014-07-30 21:52:55 +0200

    Add vim modelines
    The shiftwidth is whatever the tab stop is, the tab stop is 8, and
    various C indention rules are followed: don't indent case labels beyond
    switch statements, don't indent function declarations, indent
    continuation lines with four spaces.

--- src/callbacks.c
+++ src/callbacks.c
@@ -1,3 +1,5 @@
+/* vim: set sw=0 ts=8 cinoptions=:0,t0,+4,(4: */
+
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
--- src/pathnames.h
+++ src/pathnames.h
@@ -1,1 +1,3 @@
+/* vim: set sw=0 ts=8 cinoptions=:0,t0,+4,(4: */
+
 #define INTERFACE_PATH DATADIR"/argonaut.ui"
